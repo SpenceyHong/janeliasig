d2963aa5613423e7cbb38b1d3005ac3e1afcf41b
Spencer Hong <spencer.hongx@gmail.com>
2015-08-06 15:40:13 -0400

    d

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
 		return f.read()
 
 setup(name='janeliasig',
-      version='0.92',
+      version='0.93',
       description='Rudimentary Simulations/Manipulations of PMT Outputs',
       long_description = readme(),
       classifiers = [
